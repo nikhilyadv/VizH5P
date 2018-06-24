19ac178312ba6611e9c884808c0dcdd67f85dc4b
Nikhil Kumar Yadav <nikhilk.yadav22@gmail.com>
2018-06-25 03:57:46 +0530

    Capture and Save in DB

---
+++ src/savetoDB.php
@@ -0,0 +1,52 @@
+<?php	  
+namespace Drupal\vizh5p;
+
+use Drupal\Core\Database\Connection;
+
+/**
+ * Defines a storage handler class that handles the node grants system.
+ * This is used to build node query access.	
+ * @ingroup mypage
+ */
+
+class savetoDB {
+
+	/**
+	 * The database connection.
+	 * @var \Drupal\Core\Database\Connection
+	 */
+
+	protected $database;  
+	/**
+	 * Constructs a MyPageDbLogic object.	  
+	 * @param \Drupal\Core\Database\Connection $database
+	 *   The database connection.
+	 */
+
+	// The $database variable came to us from the service argument.
+	public function __construct(Connection $database) {
+		$this->database = $database;
+	}	  
+
+	/**
+	 * Add new record in table VizH5P.
+	 */
+
+	public function add($content_id , $actor , $verb) {
+		if (empty($content_id) || empty($actor) || empty($verb)){
+			return FALSE;
+		}
+		$date = date('Y-m-d H:i:s');
+		//Select database using drupal inbuit APIs
+		$query = $this->database->insert('vizh5p');         
+		//Add fields
+		$query->fields(array(
+			'time' => $date,
+			'content_id' => $content_id,
+			'actor' => $actor,
+			'verb' => $verb
+		));
+		//Execute query
+		return $query->execute();
+	}
+}
